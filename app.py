2fb52fbf2382950561044f02b7e11fcc9f72b7b8
ff <vo>
2022-03-16 18:53:38 +0100

    preselected date and list for hours

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request
+from datetime import date
 
 app = Flask(__name__)
+
+months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+          'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+
 
 
 @app.route('/')
 def landing():
-    return render_template('index.html')
+
+    hours = []
+    for i in range(11, 14, 1):
+        for j in range(0, 60, 10):
+            hours.append(f"{i}:{j:02d}")
+
+    today = date.today()
+    day = today.strftime('%d')
+    month = months[int(today.strftime('%m')) - 1]
+    year = today.strftime('%Y')
+    now = f'{day} {month} {year}'
+
+    return render_template('index.html', now=now, hours=hours)
 
 
 @app.route('/reservation')
